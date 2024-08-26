5cd446f3b6ae7e0a3c473a5da64d5cc817a9aea3
Daniel Freytag <daniel@frytg.com>
2024-08-26 15:32:59 +0200

    fix: linter errors

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,40 +1,40 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  mode: 'production',
-  publicDir: false,
-  base: '/compiled',
+	mode: 'production',
+	publicDir: false,
+	base: '/compiled',
 
-  build: {
-    // set primary output and asset config
-    outDir: resolve(__dirname, './assets/css/compiled/'),
-    emptyOutDir: true,
+	build: {
+		// set primary output and asset config
+		outDir: resolve(__dirname, './assets/css/compiled/'),
+		emptyOutDir: true,
 
-    // generate manifest.json in outDir
-    manifest: 'manifest.json',
+		// generate manifest.json in outDir
+		manifest: 'manifest.json',
 
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 1000,
+		minify: 'esbuild',
+		chunkSizeWarningLimit: 1000,
 
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, './assets/css/main-tailwind-compiled.css'),
-        fonts: resolve(__dirname, './assets/css/fonts.scss'),
-      },
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, './assets/css/main-tailwind-compiled.css'),
+				fonts: resolve(__dirname, './assets/css/fonts.scss'),
+			},
 
-      makeAbsoluteExternalsRelative: true,
+			makeAbsoluteExternalsRelative: true,
 
-      output: {
-        dir: 'static/compiled',
-        entryFileNames: 'entry/[name]-[hash].js',
-        chunkFileNames: 'chunks/[name]-[hash].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
+			output: {
+				dir: 'static/compiled',
+				entryFileNames: 'entry/[name]-[hash].js',
+				chunkFileNames: 'chunks/[name]-[hash].js',
+				assetFileNames: '[name].[ext]',
+			},
+		},
 
-    sourcemap: false,
-  },
+		sourcemap: false,
+	},
 
-  devSourcemap: false,
-});
+	devSourcemap: false,
+})
